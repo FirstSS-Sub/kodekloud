de6ad59876a00b5b34ffbddeddb21fa3ec655ede
saito-sota-ab <saito_sota@applibot.co.jp>
2022-08-14 23:51:42 +0900

    kube-config.sh set-credentials 追記

--- kubernetes-challenge-1/kube-config.sh
+++ kubernetes-challenge-1/kube-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
   
 kubectl config set-context developer --user=martin --cluster=kubernetes && \
-        kubectl config use-context developer
+        kubectl config use-context developer && \
+        kubectl config set-credentials martin --client-key=/root/martin.key --client-certificate=/root/martin.crt
